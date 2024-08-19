031e806a73dcb89790df33af59e681dbcfdf2cc8
zackees <z@zackees.com>
2024-08-19 10:06:55 -0700

    Update get_config_path function to migrate users smoothly

--- src/aicode/openaicfg.py
+++ src/aicode/openaicfg.py
@@ -1,13 +1,37 @@
 import json
 import os
+import shutil
+import warnings
 
 from appdirs import user_config_dir  # type: ignore
 
 
-def get_config_path() -> str:
+def get_config_path_legacy() -> str:
+    env_path = user_config_dir("zcmds", "zcmds", roaming=True)
+    config_file = os.path.join(env_path, "openai.json")
+    return config_file
+
+
+def get_config_path_new() -> str:
     env_path = user_config_dir("aicode", roaming=True)
     config_file = os.path.join(env_path, "openai.json")
     return config_file
+
+
+def get_config_path() -> str:
+    """Gently migrate users to the new config path."""
+    config_path = get_config_path_new()
+    if os.path.exists(config_path):
+        return config_path
+    config_path = get_config_path_legacy()
+    if os.path.exists(config_path):
+        try:
+            shutil.copy(config_path, get_config_path_new())
+        except OSError:
+            warnings.warn(
+                f"Failed to copy config file from {config_path} to {get_config_path_new()}"
+            )
+    return get_config_path_new()
 
 
 def save_config(config: dict) -> None:
