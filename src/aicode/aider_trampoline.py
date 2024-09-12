70b288adf5576db1bb5e2708dcc947f66c6d52e0
zackees <z@zackees.com>
2024-09-11 17:32:21 -0700

    Update version to 1.2.13 and handle KeyboardInterrupt

--- src/aicode/aider_trampoline.py
+++ src/aicode/aider_trampoline.py
@@ -5,15 +5,18 @@ import sys
 
 def main() -> int:
     """Main function."""
-    cwd = sys.argv[1]
-    os.chdir(cwd)
-    sys.argv.pop(1)
-    # print(f"Working directory: {cwd}")
-    os.chdir(cwd)
-    args = subprocess.list2cmdline(sys.argv[1:])
-    # print(f"Running: {args}")
-    cp = subprocess.run(args, shell=True)
-    return cp.returncode
+    prev_cwd = os.getcwd()
+    try:
+        cwd = sys.argv[1]
+        os.chdir(cwd)
+        sys.argv.pop(1)
+        args = subprocess.list2cmdline(sys.argv[1:])
+        cp = subprocess.run(args, shell=True)
+        return cp.returncode
+    except KeyboardInterrupt:
+        return 1
+    finally:
+        os.chdir(prev_cwd)
 
 
 if __name__ == "__main__":
