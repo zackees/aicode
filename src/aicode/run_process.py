fb25014c7c7947e098ce6e890964f7c0c335bdd1
zackees <z@zackees.com>
2025-02-25 14:07:28 -0800

    chore: bump version to 2.0.17 and update run_process docstring

--- src/aicode/run_process.py
+++ src/aicode/run_process.py
@@ -1,4 +1,5 @@
 def run_process(cmd_list: list[str]) -> int:
+    """Todo this will be replaced with something more interactive."""
     from aicode.aider_control import aider_run
 
     rtn = aider_run(cmd_list).returncode
